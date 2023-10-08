76a1ebd9ee134c14763da79d3e9a696e90fe77c8
sidney31 <surovbogdan05@gmail.com>
2023-10-08 20:44:37 +0300

    add autoReloader script

---
+++ autoReloader.lua
@@ -0,0 +1,6 @@
+require 'lib.sampfuncs'
+function onSystemMessage(msg, type, script)
+    if msg:find('farm_helper_v3.lua:%d+:.+') and type == 3 then
+            script.load("farm_helper_v3.lua")
+        end
+end
